b2cf99737948b4ad71a4c57c4251a93d0e7a64dc
shijting <09076712@hcldap.intra>
2024-01-23 15:11:41 +0800

    优化option

--- option/option.go
+++ option/option.go
@@ -4,6 +4,7 @@ type Option[T any] func(t *T)
 
 type Options[T any] []Option[T]
 
+// Apply applies all options to t.
 func (opts Options[T]) Apply(t *T) {
 	for _, opt := range opts {
 		opt(t)
@@ -14,6 +15,9 @@ type OptionErr[T any] func(t *T) error
 
 type OptionsErr[T any] []OptionErr[T]
 
+// Apply applies all options to t.
+// Returns an error if any of the options returns an error.
+// Returns nil if no error occurred.
 func (opts OptionsErr[T]) Apply(t *T) error {
 	for _, opt := range opts {
 		if err := opt(t); err != nil {
