d1351bfc291feaace2e6527abd87969269d4153e
shijting <09076712@hcldap.intra>
2024-01-23 16:11:02 +0800

    增加指数退避重试策略&&随机间隔重试策略

--- lockx/retry.go
+++ lockx/retry.go
@@ -1,6 +1,9 @@
 package lockx
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 // RetryStrategy 重试策略
 type RetryStrategy interface {
@@ -25,4 +28,66 @@ func NewFixIntervalRetry(interval time.Duration, max int, cnt int) *FixIntervalR
 func (f FixIntervalRetry) Next() (time.Duration, bool) {
 	f.cnt++
 	return f.Interval, f.cnt <= f.Max
+}
+
+// ExponentialBackoffRetry 指数退避重试
+type ExponentialBackoffRetry struct {
+	// 初始间隔
+	InitialInterval time.Duration
+	// 最大间隔
+	MaxInterval time.Duration
+	// 最大次数
+	MaxAttempts int
+	// 当前次数，从 0 开始，表示第一次重试，第二次重试，第三次重试，以此类推。
+	CurrentAttempt int
+}
+
+func NewExponentialBackoffRetry(initialInterval, maxInterval time.Duration, maxAttempts int) *ExponentialBackoffRetry {
+	return &ExponentialBackoffRetry{
+		InitialInterval: initialInterval,
+		MaxInterval:     maxInterval,
+		MaxAttempts:     maxAttempts,
+		CurrentAttempt:  0,
+	}
+}
+
+func (e *ExponentialBackoffRetry) Next() (time.Duration, bool) {
+	if e.CurrentAttempt >= e.MaxAttempts {
+		return 0, false
+	}
+
+	interval := e.InitialInterval * time.Duration(1<<uint(e.CurrentAttempt))
+	if interval > e.MaxInterval {
+		interval = e.MaxInterval
+	}
+
+	e.CurrentAttempt++
+	return interval, true
+}
+
+// RandomizedRetry 随机间隔重试
+type RandomizedRetry struct {
+	MaxInterval    time.Duration
+	MaxAttempts    int
+	CurrentAttempt int
+}
+
+func NewRandomizedRetry(maxInterval time.Duration, maxAttempts int) *RandomizedRetry {
+	return &RandomizedRetry{
+		MaxInterval:    maxInterval,
+		MaxAttempts:    maxAttempts,
+		CurrentAttempt: 0,
+	}
+}
+
+func (r *RandomizedRetry) Next() (time.Duration, bool) {
+	if r.CurrentAttempt >= r.MaxAttempts {
+		return 0, false
+	}
+
+	// 生成一个随机的等待时间
+	randomInterval := time.Duration(rand.Int63n(int64(r.MaxInterval)))
+
+	r.CurrentAttempt++
+	return randomInterval, true
 }
