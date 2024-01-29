618ab4019721dca07b4a723a869226b698b932b5
shijitning <1269879928@qq.com>
2024-01-29 22:02:08 +0800

    令牌桶限流器

---
+++ limiter/bucket.go
@@ -0,0 +1,58 @@
+package limiter
+
+import (
+	"sync"
+	"time"
+)
+
+// Bucket 令牌桶限流器
+type Bucket struct {
+	// cap 桶容量
+	cap int64
+	// tokens 桶中的令牌数
+	tokens int64
+	// rate 令牌产生速率
+	rate int64
+	// 上次执行rate计算的时间
+	last int64
+	l    sync.Mutex
+}
+
+func NewBucket(cap, rate int64) *Bucket {
+	if cap <= 0 || rate <= 0 {
+		panic("cap and rate must be greater than 0")
+	}
+	return &Bucket{cap: cap, tokens: cap, rate: rate}
+}
+
+// add 添加令牌
+//func (b *Bucket) add() {
+//	b.l.Lock()
+//	defer b.l.Unlock()
+//
+//	if b.tokens+b.rate < b.cap {
+//		b.tokens += b.rate
+//	} else {
+//		b.tokens = b.cap
+//	}
+//}
+
+// Accept 是否允许通过
+func (b *Bucket) Accept() bool {
+	b.l.Lock()
+	defer b.l.Unlock()
+
+	now := time.Now().Unix()
+	// 计算当前应该添加的令牌数
+	b.tokens += (now - b.last) * b.rate
+	if b.tokens > b.cap {
+		b.tokens = b.cap
+	}
+
+	if b.tokens <= 0 {
+		return false
+	}
+
+	b.tokens--
+	return true
+}
