d1bd170a08bf54f0fe03a42647fa921db94cb591
shijitning <1269879928@qq.com>
2024-01-29 23:22:02 +0800

    lru缓存增加删除最后一个元素

--- LRU.go
+++ LRU.go
@@ -18,17 +18,27 @@ func NewLRUCache[K comparable, V any]() *LRUCache[K, V] {
 	}
 }
 
-func (l *LRUCache[K, V]) Get(key K) (value V, ok bool) {
+type Data[K comparable, V any] struct {
+	Key   K
+	Value V
+	//	TODO 过期时间
+}
+
+func NewData[K comparable, V any](key K, value V) Data[K, V] {
+	return Data[K, V]{Key: key, Value: value}
+}
+
+func (l *LRUCache[K, V]) Get(key K) (value Data[K, V], ok bool) {
 	if v, ok := l.data.Load(key); ok {
 		// 移动到最前面
 		l.list.MoveToFront(v)
-		return v.Value.(V), true
+		return v.Value.(Data[K, V]), true
 	}
-	var v V
+	var v Data[K, V]
 	return v, false
 }
 
-func (l *LRUCache[K, V]) Set(key K, value V) {
+func (l *LRUCache[K, V]) Set(key K, value Data[K, V]) {
 	if v, ok := l.data.Load(key); ok {
 		v.Value = value
 		// 移动到最前面
@@ -36,6 +46,16 @@ func (l *LRUCache[K, V]) Set(key K, value V) {
 	} else {
 		// 新增 并且移动到最前面
 		l.data.Store(key, l.list.PushFront(value))
+	}
+}
+
+// RemoveBack 删除最后一个
+func (l *LRUCache[K, V]) RemoveBack() {
+	ele := l.list.Back()
+	if ele != nil {
+		l.list.Remove(ele)
+		key := ele.Value.(Data[K, V]).Key
+		l.data.LoadAndDelete(key)
 	}
 }
 
